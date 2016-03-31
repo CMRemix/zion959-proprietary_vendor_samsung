5f72b4f028cf50eeeff576430bcedf92d55a26a7
Pafcholini <nadyaivanova14@gmail.com>
2016-03-31 20:57:58 +0200

    trltespr: update
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- trltespr/trltespr-vendor-blobs.mk
+++ trltespr/trltespr-vendor-blobs.mk
@@ -19,8 +19,15 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trltespr/proprietary/vendor/firmware/bcm43569.hcd:system/vendor/firmware/bcm43569.hcd \
     vendor/samsung/trltespr/proprietary/vendor/firmware/bcm43569A0.hcd:system/vendor/firmware/bcm43569A0.hcd \
     vendor/samsung/trltespr/proprietary/vendor/firmware/bcm43569A1.hcd:system/vendor/firmware/bcm43569A1.hcd \
-    vendor/samsung/trltespr/proprietary/lib/hw/bluetooth.default.so:system/lib/hw/bluetooth.default.so \
-    vendor/samsung/trltespr/proprietary/lib/libbluetooth_jni.so:system/lib/libbluetooth_jni.so \
+    vendor/samsung/trltespr/proprietary/bin/gsiff_daemon:system/bin/gsiff_daemon \
+    vendor/samsung/trltespr/proprietary/etc/permissions/com.qualcomm.location.xml:system/etc/permissions/com.qualcomm.location.xml \
+    vendor/samsung/trltespr/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
+    vendor/samsung/trltespr/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/libizat_core.so:system/vendor/lib/libizat_core.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/liblocationservice.so:system/vendor/lib/liblocationservice.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/libulp2.so:system/vendor/lib/libulp2.so \
     vendor/samsung/trltespr/proprietary/vendor/firmware/sec_s3fwrn5_firmware.bin:system/vendor/firmware/sec_s3fwrn5_firmware.bin \
     vendor/samsung/trltespr/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
     vendor/samsung/trltespr/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
@@ -31,17 +38,21 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trltespr/proprietary/vendor/lib/libqmi_client_qmux.so:system/vendor/lib/libqmi_client_qmux.so \
     vendor/samsung/trltespr/proprietary/vendor/lib/libqmi_common_so.so:system/vendor/lib/libqmi_common_so.so \
     vendor/samsung/trltespr/proprietary/vendor/lib/libqmi_csi.so:system/vendor/lib/libqmi_csi.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/libqmi_csvt_srvc.so:system/vendor/lib/libqmi_csvt_srvc.so \
     vendor/samsung/trltespr/proprietary/vendor/lib/libqmi_encdec.so:system/vendor/lib/libqmi_encdec.so \
     vendor/samsung/trltespr/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/samsung/trltespr/proprietary/bin/efsks:system/bin/efsks \
+    vendor/samsung/trltespr/proprietary/bin/ds_fmc_appd:system/bin/ds_fmc_appd \
     vendor/samsung/trltespr/proprietary/bin/ks:system/bin/ks \
     vendor/samsung/trltespr/proprietary/bin/mdm_helper:system/bin/mdm_helper \
     vendor/samsung/trltespr/proprietary/bin/mdm_helper_proxy:system/bin/mdm_helper_proxy \
     vendor/samsung/trltespr/proprietary/bin/qcks:system/bin/qcks \
     vendor/samsung/trltespr/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/samsung/trltespr/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/samsung/trltespr/proprietary/bin/rild:system/bin/rild \
+    vendor/samsung/trltespr/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/samsung/trltespr/proprietary/bin/rmnetcli:system/bin/rmnetcli \
-    vendor/samsung/trltespr/proprietary/lib/libcfiril.so:system/lib/libcfiril.so \
+    vendor/samsung/trltespr/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/trltespr/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/trltespr/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/trltespr/proprietary/lib/librmnetctl.so:system/lib/librmnetctl.so \
@@ -50,8 +61,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trltespr/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/trltespr/proprietary/lib/librilswitch.so:system/lib/librilswitch.so \
     vendor/samsung/trltespr/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
-    vendor/samsung/trltespr/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
-    vendor/samsung/trltespr/proprietary/vendor/lib/libmdmdetect.so:system/vendor/lib/libmdmdetect.so \
+    vendor/samsung/trltespr/proprietary/lib/libcfiril.so:system/lib/libcfiril.so \
     vendor/samsung/trltespr/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
-    vendor/samsung/trltespr/proprietary/vendor/lib/libtime_genoff.so:system/vendor/lib/libtime_genoff.so \
-    vendor/samsung/trltespr/proprietary/vendor/lib/libxml.so:system/vendor/lib/libxml.so
+    vendor/samsung/trltespr/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/libxml.so:system/vendor/lib/libxml.so \
+    vendor/samsung/trltespr/proprietary/vendor/lib/libtime_genoff.so:system/vendor/lib/libtime_genoff.so
